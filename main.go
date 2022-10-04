6b3d8024c1b0a4c1dcb0ca25d3dab1fe6c68d5c4
Rumi Allbert <rumiallbert@gmail.com>
2022-10-04 14:31:42 -0400

    add imports

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import ()
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type MongoInstance struct {
-	Client
-	Db
+	Client *mongo.Client
+	Db     *mongo.Database
 }
 
 var mg MongoInstance
